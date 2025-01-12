a913262c5a7ea3f1601cde6b230bb8a18ad167b9
Ginxss <git.timo.friedrich@pm.me>
2025-01-12 19:00:54 +0100

    files tests

--- src/files.rs
+++ src/files.rs
@@ -1,22 +1,156 @@
-use std::{
-    fs::{self, DirEntry},
-    path::PathBuf,
-};
+use std::fs::{self, DirEntry};
 
-pub fn get_valid_child_dirs(path: PathBuf) -> impl Iterator<Item = DirEntry> {
-    fs::read_dir(&path)
-        .expect(&format!("Error reading directory: {:#?}", path))
-        .filter_map(Result::ok)
-        .filter(|entry| {
-            entry.path().is_dir() && !entry.file_name().to_string_lossy().starts_with(".")
-        })
+pub fn get_files(path: &str) -> Vec<DirEntry> {
+    get_dir_entries(path)
+        .filter(|entry| entry.path().is_file())
+        .collect()
 }
 
-pub fn get_valid_child_files(path: PathBuf) -> impl Iterator<Item = DirEntry> {
-    fs::read_dir(&path)
+pub fn get_dirs(path: &str) -> Vec<DirEntry> {
+    get_dir_entries(path)
+        .filter(|entry| entry.path().is_dir())
+        .collect()
+}
+
+fn get_dir_entries(path: &str) -> impl Iterator<Item = DirEntry> {
+    fs::read_dir(path)
         .expect(&format!("Error reading directory: {:#?}", path))
         .filter_map(Result::ok)
-        .filter(|entry| {
-            entry.path().is_file() && !entry.file_name().to_string_lossy().starts_with(".")
-        })
+        .filter(|entry| !is_hidden(entry))
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name().to_string_lossy().starts_with(".")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_files_contents() {
+        let test_dir = "./test_data";
+
+        let mut file_contents: Vec<String> = get_files(test_dir)
+            .iter()
+            .map(|file| fs::read_to_string(file.path()).unwrap())
+            .collect();
+
+        assert_eq!(file_contents.len(), 3);
+
+        let path = |name: &str| [test_dir, name].join("/");
+        let mut expected_file_contents = vec![
+            fs::read_to_string(path("file1")).unwrap(),
+            fs::read_to_string(path("file2")).unwrap(),
+            fs::read_to_string(path("file3")).unwrap(),
+        ];
+
+        file_contents.sort();
+        expected_file_contents.sort();
+        assert_eq!(file_contents, expected_file_contents);
+    }
+
+    #[test]
+    fn test_get_dirs_names() {
+        let test_dir = "./test_data";
+
+        let mut dir_names: Vec<String> = get_dirs(test_dir)
+            .iter()
+            .map(|dir| dir.file_name().into_string().unwrap())
+            .collect();
+
+        assert_eq!(dir_names.len(), 3);
+
+        let mut expected_dir_names = vec!["dir1", "dir2", "dir3"];
+
+        dir_names.sort();
+        expected_dir_names.sort();
+        assert_eq!(dir_names, expected_dir_names);
+    }
+
+    #[test]
+    fn test_get_dirs_subfiles() {
+        let test_dir = "./test_data";
+
+        let mut child_file_contents: Vec<Vec<String>> = get_dirs(test_dir)
+            .iter()
+            .map(|dir| get_files(&dir.path().to_string_lossy()))
+            .map(|files| {
+                let mut contents: Vec<String> = files
+                    .iter()
+                    .map(|file| fs::read_to_string(file.path()).unwrap())
+                    .collect();
+                contents.sort();
+
+                return contents;
+            })
+            .collect();
+
+        assert_eq!(child_file_contents.len(), 3);
+        assert!(child_file_contents.iter().all(|c| c.len() == 3));
+
+        let path = |dir: &str, name: &str| [test_dir, dir, name].join("/");
+
+        let mut contents1 = vec![
+            fs::read_to_string(path("dir1", "dir1_file1")).unwrap(),
+            fs::read_to_string(path("dir1", "dir1_file2")).unwrap(),
+            fs::read_to_string(path("dir1", "dir1_file3")).unwrap(),
+        ];
+        contents1.sort();
+
+        let mut contents2 = vec![
+            fs::read_to_string(path("dir2", "dir2_file1")).unwrap(),
+            fs::read_to_string(path("dir2", "dir2_file2")).unwrap(),
+            fs::read_to_string(path("dir2", "dir2_file3")).unwrap(),
+        ];
+        contents2.sort();
+
+        let mut contents3 = vec![
+            fs::read_to_string(path("dir3", "dir3_file1")).unwrap(),
+            fs::read_to_string(path("dir3", "dir3_file2")).unwrap(),
+            fs::read_to_string(path("dir3", "dir3_file3")).unwrap(),
+        ];
+        contents3.sort();
+
+        let mut expected_child_file_contents = vec![contents1, contents2, contents3];
+
+        child_file_contents.sort();
+        expected_child_file_contents.sort();
+        assert_eq!(child_file_contents, expected_child_file_contents);
+    }
+
+    #[test]
+    fn test_get_dir_entries_names() {
+        let test_dir = "./test_data";
+
+        let mut entry_names: Vec<String> = get_dir_entries(test_dir)
+            .map(|dir| dir.file_name().into_string().unwrap())
+            .collect();
+
+        assert_eq!(entry_names.len(), 6);
+
+        let mut expected_entry_names = vec!["dir1", "dir2", "dir3", "file1", "file2", "file3"];
+
+        entry_names.sort();
+        expected_entry_names.sort();
+        assert_eq!(entry_names, expected_entry_names);
+    }
+
+    #[test]
+    fn test_is_hidden_names() {
+        let test_dir = "./test_data";
+
+        let mut hidden_entry_names: Vec<String> = fs::read_dir(test_dir)
+            .unwrap()
+            .filter_map(Result::ok)
+            .filter(|entry| is_hidden(entry))
+            .map(|entry| entry.file_name().into_string().unwrap())
+            .collect();
+
+        let mut expected_hidden_entry_names = vec![".hidden_dir", ".hidden_file"];
+
+        hidden_entry_names.sort();
+        expected_hidden_entry_names.sort();
+        assert_eq!(hidden_entry_names, expected_hidden_entry_names);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,183 +10,182 @@ mod args;
 mod board;
 mod files;
 
-use std::{
-    env,
-    fs::{self, DirEntry},
-    path::PathBuf,
-    vec,
-};
-
-use prettytable::{row, Table};
-
-use board::{
-    flop_height::{is_1bw, is_2bw, is_3bw, is_low, is_middling},
-    suits::{is_monotone, is_rainbow, is_twotone},
-};
-
-const DATA_DIR: &str = "./data";
-
-#[derive(Debug)]
-struct Data {
-    board: String,
-    size: Betsize,
-    eq: f32,
-    ev: f32,
-    bet_freq: f32,
-    check_freq: f32,
-}
-
+//use std::{
+//    fs::{self, DirEntry},
+//    path::PathBuf,
+//    vec,
+//};
+//
+//use prettytable::{row, Table};
+//
+//use board::{
+//    flop_height::{is_1bw, is_2bw, is_3bw, is_low, is_middling},
+//    suits::{is_monotone, is_rainbow, is_twotone},
+//};
+//
+//const DATA_DIR: &str = "./data";
+//
+//#[derive(Debug)]
+//struct Data {
+//    board: String,
+//    size: Betsize,
+//    eq: f32,
+//    ev: f32,
+//    bet_freq: f32,
+//    check_freq: f32,
+//}
+//
 fn main() {
-    let Args {
-        categories,
-        modifiers,
-        betsizes,
-        positions,
-        actions,
-    } = read_cmd_args();
-
-    let datas = build_datas(&positions, &betsizes, &actions, &categories, &modifiers);
-
-    print_table(&datas);
+    //let Args {
+    //    categories,
+    //    modifiers,
+    //    betsizes,
+    //    positions,
+    //    actions,
+    //} = read_cmd_args();
+    //
+    //let datas = build_datas(&positions, &betsizes, &actions, &categories, &modifiers);
+    //
+    //print_table(&datas);
 }
 
-fn build_datas(
-    positions: &Positions,
-    betsizes: &Vec<Betsize>,
-    actions: &Vec<String>,
-    categories: &Vec<BoardCategory>,
-    modifiers: &Vec<Modifier>,
-) -> Vec<Data> {
-    let pos_dir = files::get_valid_child_dirs(PathBuf::from(DATA_DIR))
-        .find(|dir| {
-            let name = get_filename(dir).to_lowercase();
-            name.contains(&positions.0) && name.contains(&positions.1)
-        })
-        .expect("Could not find position directory");
-
-    let collect_1bw = categories.contains(&BoardCategory::SingleBW);
-    let collect_2bw = categories.contains(&BoardCategory::DoubleBW);
-    let collect_3bw = categories.contains(&BoardCategory::TripleBW);
-    let collect_mid = categories.contains(&BoardCategory::Middling);
-    let collect_low = categories.contains(&BoardCategory::Low);
-
-    let collect_rb = modifiers.contains(&Modifier::Rainbow);
-    let collect_tt = modifiers.contains(&Modifier::Twotone);
-    let collect_mono = modifiers.contains(&Modifier::Montone);
-
-    let datas: Vec<Data> = betsizes
-        .iter()
-        .map(|size| {
-            let size_dir = files::get_valid_child_dirs(pos_dir.path())
-                .find(|size_dir| get_filename(&size_dir) == *size)
-                .expect("Could not find size directory");
-
-            let action_file =
-                find_action_file(&size_dir, actions).expect("Could not find action file");
-
-            let file_content =
-                fs::read_to_string(action_file.path()).expect("Could not read file content");
-
-            let datas = file_content
-                .lines()
-                .skip(1)
-                .map(|line| build_data_from_line(line, size))
-                .filter(|data| match categories.len() {
-                    0 => true,
-                    _ => {
-                        collect_1bw && is_1bw(&data.board)
-                            || collect_2bw && is_2bw(&data.board)
-                            || collect_3bw && is_3bw(&data.board)
-                            || collect_mid && is_middling(&data.board)
-                            || collect_low && is_low(&data.board)
-                    }
-                })
-                .filter(|data| match modifiers.len() {
-                    0 => true,
-                    _ => {
-                        collect_rb && is_rainbow(&data.board)
-                            || collect_tt && is_twotone(&data.board)
-                            || collect_mono && is_monotone(&data.board)
-                    }
-                });
-
-            let count = datas.clone().count() as f32;
-
-            let calculated = datas
-                .reduce(|data1, data2| Data {
-                    board: data1.board,
-                    size: data1.size,
-                    eq: data1.eq + data2.eq,
-                    ev: data1.ev + data2.ev,
-                    bet_freq: data1.bet_freq + data2.bet_freq,
-                    check_freq: data1.check_freq + data2.check_freq,
-                })
-                .map(|added_data| Data {
-                    board: added_data.board,
-                    size: added_data.size,
-                    eq: added_data.eq / count,
-                    ev: added_data.ev / count,
-                    bet_freq: added_data.bet_freq / count,
-                    check_freq: added_data.check_freq / count,
-                })
-                .expect("Could not calculate results");
-
-            calculated
-        })
-        .collect();
-
-    assert!(betsizes.len() == datas.len());
-
-    datas
-}
-
-fn print_table(datas: &Vec<Data>) {
-    let mut table = Table::new();
-
-    table.add_row(row!["Size", "EQ", "EV", "Bet", "Check"]);
-
-    datas
-        .iter()
-        .map(|data| row![data.size, data.eq, data.ev, data.bet_freq, data.check_freq])
-        .for_each(|row| {
-            table.add_row(row);
-        });
-
-    table.printstd();
-}
-
-fn get_filename(entry: &DirEntry) -> String {
-    let os_name = entry.file_name();
-    return os_name.to_string_lossy().to_lowercase();
-}
-
-fn find_action_file(size_dir: &DirEntry, line: &Vec<String>) -> Option<DirEntry> {
-    files::get_valid_child_files(size_dir.path()).find(|file| match file.path().file_stem() {
-        Some(name) => {
-            let split_pattern = '_';
-            let name = name.to_string_lossy();
-
-            return name.matches(split_pattern).count() == line.len()
-                && name
-                    .split(split_pattern)
-                    .skip(1)
-                    .zip(line.iter())
-                    .all(|(name_part, action)| name_part == action.as_str());
-        }
-        None => false,
-    })
-}
-
-fn build_data_from_line(line: &str, size: &str) -> Data {
-    let split: Vec<&str> = line.split('\t').collect();
-    assert!(split.len() >= 5);
-
-    Data {
-        board: split[0].to_string(),
-        size: size.to_string(),
-        eq: split[1].parse().expect("Equity needs to be a number"),
-        ev: split[2].parse().expect("EV needs to be a number"),
-        bet_freq: split[3].parse().expect("Bet freq. needs to be a number"),
-        check_freq: split[4].parse().expect("Check freq. needs to be a number"),
-    }
-}
+//fn build_datas(
+//    positions: &Positions,
+//    betsizes: &Vec<Betsize>,
+//    actions: &Vec<String>,
+//    categories: &Vec<BoardCategory>,
+//    modifiers: &Vec<Modifier>,
+//) -> Vec<Data> {
+//    let pos_dir = files::get_valid_child_dirs(PathBuf::from(DATA_DIR))
+//        .find(|dir| {
+//            let name = get_filename(dir).to_lowercase();
+//            name.contains(&positions.0) && name.contains(&positions.1)
+//        })
+//        .expect("Could not find position directory");
+//
+//    let collect_1bw = categories.contains(&BoardCategory::SingleBW);
+//    let collect_2bw = categories.contains(&BoardCategory::DoubleBW);
+//    let collect_3bw = categories.contains(&BoardCategory::TripleBW);
+//    let collect_mid = categories.contains(&BoardCategory::Middling);
+//    let collect_low = categories.contains(&BoardCategory::Low);
+//
+//    let collect_rb = modifiers.contains(&Modifier::Rainbow);
+//    let collect_tt = modifiers.contains(&Modifier::Twotone);
+//    let collect_mono = modifiers.contains(&Modifier::Montone);
+//
+//    let datas: Vec<Data> = betsizes
+//        .iter()
+//        .map(|size| {
+//            let size_dir = files::get_valid_child_dirs(pos_dir.path())
+//                .find(|size_dir| get_filename(&size_dir) == *size)
+//                .expect("Could not find size directory");
+//
+//            let action_file =
+//                find_action_file(&size_dir, actions).expect("Could not find action file");
+//
+//            let file_content =
+//                fs::read_to_string(action_file.path()).expect("Could not read file content");
+//
+//            let datas = file_content
+//                .lines()
+//                .skip(1)
+//                .map(|line| build_data_from_line(line, size))
+//                .filter(|data| match categories.len() {
+//                    0 => true,
+//                    _ => {
+//                        collect_1bw && is_1bw(&data.board)
+//                            || collect_2bw && is_2bw(&data.board)
+//                            || collect_3bw && is_3bw(&data.board)
+//                            || collect_mid && is_middling(&data.board)
+//                            || collect_low && is_low(&data.board)
+//                    }
+//                })
+//                .filter(|data| match modifiers.len() {
+//                    0 => true,
+//                    _ => {
+//                        collect_rb && is_rainbow(&data.board)
+//                            || collect_tt && is_twotone(&data.board)
+//                            || collect_mono && is_monotone(&data.board)
+//                    }
+//                });
+//
+//            let count = datas.clone().count() as f32;
+//
+//            let calculated = datas
+//                .reduce(|data1, data2| Data {
+//                    board: data1.board,
+//                    size: data1.size,
+//                    eq: data1.eq + data2.eq,
+//                    ev: data1.ev + data2.ev,
+//                    bet_freq: data1.bet_freq + data2.bet_freq,
+//                    check_freq: data1.check_freq + data2.check_freq,
+//                })
+//                .map(|added_data| Data {
+//                    board: added_data.board,
+//                    size: added_data.size,
+//                    eq: added_data.eq / count,
+//                    ev: added_data.ev / count,
+//                    bet_freq: added_data.bet_freq / count,
+//                    check_freq: added_data.check_freq / count,
+//                })
+//                .expect("Could not calculate results");
+//
+//            calculated
+//        })
+//        .collect();
+//
+//    assert!(betsizes.len() == datas.len());
+//
+//    datas
+//}
+//
+//fn print_table(datas: &Vec<Data>) {
+//    let mut table = Table::new();
+//
+//    table.add_row(row!["Size", "EQ", "EV", "Bet", "Check"]);
+//
+//    datas
+//        .iter()
+//        .map(|data| row![data.size, data.eq, data.ev, data.bet_freq, data.check_freq])
+//        .for_each(|row| {
+//            table.add_row(row);
+//        });
+//
+//    table.printstd();
+//}
+//
+//fn get_filename(entry: &DirEntry) -> String {
+//    let os_name = entry.file_name();
+//    return os_name.to_string_lossy().to_lowercase();
+//}
+//
+//fn find_action_file(size_dir: &DirEntry, line: &Vec<String>) -> Option<DirEntry> {
+//    files::get_valid_child_files(size_dir.path()).find(|file| match file.path().file_stem() {
+//        Some(name) => {
+//            let split_pattern = '_';
+//            let name = name.to_string_lossy();
+//
+//            return name.matches(split_pattern).count() == line.len()
+//                && name
+//                    .split(split_pattern)
+//                    .skip(1)
+//                    .zip(line.iter())
+//                    .all(|(name_part, action)| name_part == action.as_str());
+//        }
+//        None => false,
+//    })
+//}
+//
+//fn build_data_from_line(line: &str, size: &str) -> Data {
+//    let split: Vec<&str> = line.split('\t').collect();
+//    assert!(split.len() >= 5);
+//
+//    Data {
+//        board: split[0].to_string(),
+//        size: size.to_string(),
+//        eq: split[1].parse().expect("Equity needs to be a number"),
+//        ev: split[2].parse().expect("EV needs to be a number"),
+//        bet_freq: split[3].parse().expect("Bet freq. needs to be a number"),
+//        check_freq: split[4].parse().expect("Check freq. needs to be a number"),
+//    }
+//}
