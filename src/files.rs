60bbdf565ff90c3f5e1867d41d7db417aa1206e9
Ginxss <git.timo.friedrich@pm.me>
2025-05-17 17:21:42 +0200

    files tests

--- src/files.rs
+++ src/files.rs
@@ -80,22 +80,53 @@ mod tests {
     fn test_get_dirs_subfiles() {
         let test_dir = Path::new("./test_data/BTN vs BB");
 
-        let mut file_contents: Vec<String> = get_dirs(test_dir)
+        let mut file_contents: Vec<_> = get_dirs(test_dir)
             .iter()
             .map(|dir| {
-                let files = get_files(&dir.path());
+                let dir_path = &dir.path();
+
+                let files = get_files(dir_path);
                 assert_eq!(files.len(), 1);
                 let file = &files[0];
 
                 let filename = get_name(&file.path());
                 assert_eq!(filename, "after_check");
 
-                fs::read_to_string(&file.path()).unwrap()
+                (
+                    get_name(dir_path),
+                    fs::read_to_string(&file.path()).unwrap(),
+                )
             })
             .collect();
 
-        // TODO: Unterschiedliche Data & Header fuer unterschiedliche Sizes -> Und dann calculation tests!
-        let expected_content = "Tree	Equity(*)	EV	Bet 82.5	Check
+        let expected_content_33 = "Tree	Equity(*)	EV	Bet 18.15	Check
+8s8d8c	48.567	36.457	81.582	10.43	
+8s8d6d	66.065	10.514	35.860	83.144	
+Ks7d4c	37.611	30.190	28.302	71.696	
+As5s5d	81.424	32.426	7.024	62.996	
+As7d4c	50.45	53.898	13.64	79.342	
+Ts6s4d	80.414	7.857	2.348	87.656	
+";
+
+        let expected_content_50 = "Tree	Equity(*)	EV	Bet 27.5	Check
+8s8d8c	73.562	54.461	86.582	42.42	
+8s8d6d	74.060	16.549	23.860	89.143	
+Ks7d4c	27.603	62.212	15.302	68.695	
+As5s5d	50.418	36.440	4.024	93.995	
+As7d4c	24.40	62.912	13.64	99.341	
+Ts6s4d	67.407	18.865	28.348	58.653	
+";
+
+        let expected_content_75 = "Tree	Equity(*)	EV	Bet 41.25	Check
+8s8d8c	49.562	47.461	63.582	36.42	
+8s8d6d	64.060	19.549	27.860	70.143	
+Ks7d4c	36.603	41.212	15.302	67.695	
+As5s5d	86.418	54.440	4.024	81.995	
+As7d4c	51.40	58.912	16.64	93.341	
+Ts6s4d	78.407	12.865	2.348	64.653	
+";
+
+        let expected_content_150 = "Tree	Equity(*)	EV	Bet 82.5	Check
 8s8d8c	42.562	41.461	77.582	24.42	
 8s8d6d	61.060	13.549	32.860	78.143	
 Ks7d4c	33.603	34.212	24.302	61.695	
@@ -103,8 +134,13 @@ As5s5d	77.418	34.440	6.024	88.995
 As7d4c	49.40	55.912	20.64	99.341	
 Ts6s4d	70.407	10.865	3.348	77.653	
 ";
-        let mut expected_contents: Vec<String> =
-            (0..4).map(|_| expected_content.to_string()).collect();
+
+        let mut expected_contents = vec![
+            ("33".to_string(), expected_content_33.to_string()),
+            ("50".to_string(), expected_content_50.to_string()),
+            ("75".to_string(), expected_content_75.to_string()),
+            ("150".to_string(), expected_content_150.to_string()),
+        ];
 
         expected_contents.sort();
         file_contents.sort();
